0ebf32cf63c467ff5600413c86d68b89bd47bfc4
Hana Acciaioli <hana.acciaioli@gmail.com>
2023-01-23 17:43:56 -0800

    Routes setup.. 3rd times the charm

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import BookList from './components/book/BookList';
+import BookDetail from './components/book/BookDetail';
 import './App.css';
+import { Route, Link } from 'react-router-dom';
 
 function App() {
   // TODO: Add routes to books & views
   return (
     <main className="container">
-      <h1>Library Catalog</h1>
-      <BookList />
+      <Route exact path="/">
+        <h1>Library Catalog</h1>
+        <Link to="/books">Click here to see book list</Link>
+      </Route>
+      <Route exact path="/books" component={BookList} />
+      <Route path="/books/:id" component={BookDetail} />
     </main>
   );
 }
